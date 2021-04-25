eeb1d207365f5c9af6975b7a740753cc5e35663f
Shigeki Suwa <shigeki.suwa@ztrehagem.dev>
2021-04-25 12:34:51 +0900

    commit

--- public/index.js
+++ public/index.js
@@ -1,1 +1,5 @@
-console.log('index.js')
+import { WordCount } from "./components/WordCount.js";
+
+WordCount.register()
+
+Object.defineProperty(window, 'WordCount', { value: WordCount })
--- server/main.js
+++ server/main.js
@@ -1,36 +1,15 @@
 import * as http from 'http'
-import * as fs from 'fs'
-import * as path from 'path'
+import serveStatic from 'serve-static'
 
-const serveUrl = new URL('http://localhost:8080/')
+const port = 8080
+
+const staticServer = serveStatic('public')
 
 const server = http.createServer((req, res) => {
   console.log(`[${new Date().toLocaleTimeString()}] ${req.url}`)
-
-  const reqUrl = new URL(req.url, serveUrl)
-  if (reqUrl.pathname.endsWith('/')) {
-    reqUrl.pathname = reqUrl.pathname + 'index.html'
-  }
-  const staticFilePath = path.join('public', reqUrl.pathname)
-
-  const readStream = fs.createReadStream(staticFilePath)
-
-  readStream.on('error', (error) => {
-    if (error.code === 'ENOENT') {
-      res.statusCode = 404
-      res.end()
-    } else {
-      console.error('serving static file error')
-      console.error(error)
-      res.statusCode = 500
-      res.end()
-    }
-  })
-
-  res.statusCode = 200
-  readStream.pipe(res)
+  staticServer(req, res)
 })
 
-server.listen(parseInt(serveUrl.port), serveUrl.hostname, () => {
-  console.log(`listening on ${serveUrl}`)
+server.listen(port, () => {
+  console.log(`listening on ${port}`)
 })
